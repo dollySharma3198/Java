16c75561af50625b9e67cd556be2debd9c8191a8
dollySharma3198 <112879337+dollySharma3198@users.noreply.github.com>
2022-09-08 12:52:07 +0530

    employeeDetails.java

---
+++ employeeDetails.java
@@ -0,0 +1,31 @@
+// Employee details
+//error main.java 
+class Phone
+{
+    String color;//instance variables
+    float dispSize;
+    String modelNo;
+    void call()
+    {
+        System.out.println("we can call");
+    }
+    void showPhoneDetails()
+    {
+        System.out.println("Model No. :"+modelNo);
+    }
+}
+public class Main.java{
+    public static void main(String[] args) {
+        Phone amanKaPhone=new Phone(); //instantiation
+        Phone dollyKaPhone=new Phone(); //instantiation
+        
+        amanKaPhone.modelNo="Realme XT";
+        dollyKaPhone.modelNo="Nokia 3310";
+
+        amanKaPhone.showPhoneDetails();
+        dollyKaPhone.showPhoneDetails();
+
+        // System.out.println("aman's phone model No. "+amanKaPhone.modelNo);
+        // System.out.println("dolly's phone model No. "+dollyKaPhone.modelNo);
+    }
+}
