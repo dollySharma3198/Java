1f224c12817a5a6d885c79d9504e544cb86633bd
dollySharma3198 <112879337+dollySharma3198@users.noreply.github.com>
2022-09-08 14:35:34 +0530

    OppsCellPhone.java

---
+++ OppsCellPhone.java
@@ -0,0 +1,36 @@
+// CELLPHONE 
+
+import java.util.Scanner;
+ class cellPhone
+ 
+{
+    
+    public void ring()
+    {
+        System.out.println("ringigng.....");
+        
+    }
+    public void vibrate()
+    {
+        System.out.println("vibrating.....");
+        
+    }
+    public void callFriend()
+    {
+        System.out.println("calling to dolly .....");
+        
+    }
+    
+}
+     class Main
+ {
+     public static void main(String[] args){
+        
+    cellPhone samsung=new cellPhone();
+    samsung.callFriend();
+    samsung.vibrate();
+    samsung.ring();
+    
+    
+     }
+}
