642e76daf1bef93610e9a7dbe3da266444d3522f
dollySharma3198 <112879337+dollySharma3198@users.noreply.github.com>
2022-09-09 19:55:49 +0530

    ch

--- OppsSquareArea.java
+++ OppsSquareArea.java
@@ -1,24 +1,37 @@
+
 // creat a class square with a side calculating area, perimeter
 //error
 import java.util.Scanner;
- class square
- int side;
- public int area()
- {
-     return side*side;
- }
- public int perimeter()
- {
-     4*side;
- }
- 
-   public class Main {
-     public static void main(String[] args){
-        
-    square sq=new square();
-    sq.side=4;
-    System.out.println("area of square"+sq.area());
-    System.out.println( "side of square"+sq.perimeter());
 
-     }
+class square {
+
+    int side;
+
+    public int area() {
+
+        return side * side;
+
+    }
+
+    public int perimeter() {
+
+        return 4 * side;
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        square sq = new square();
+
+        sq.side = 6;
+
+        System.out.println("area of square " + sq.area());
+
+        System.out.println("side of square " + sq.perimeter());
+
+    }
 }
