d675efbfec49fb20b618f5699e22a70a380031f3
dollySharma3198 <112879337+dollySharma3198@users.noreply.github.com>
2022-11-11 17:39:32 +0530

    sets.java

---
+++ sets.java
@@ -0,0 +1,69 @@
+// import java.util.HashSet;
+// import java.util.Iterator;
+import java.util.LinkedHashSet;
+// import java.util.Set;
+// import java.util.TreeSet;
+
+class ArrayList1
+{
+    public static void main(String[] args) {
+       
+        // Set<String> sets=new HashSet<>();
+
+        // sets.add("Aman");
+        // sets.add("Dolly");
+        // sets.add("Lalit");
+        // sets.add("prince");
+        // sets.add("Aman 2");
+        // sets.add("Aman");
+        // sets.add("Aman");
+        // sets.add("piyush");
+
+        // System.out.println(sets);
+        // Iterator<String> itr =sets.iterator();
+// 
+        // while (itr.hasNext()) {
+        //     System.out.println(" element is :"+itr.next());
+        // }
+
+        // Object arr[]=sets.toArray();
+        
+        // System.out.println(arr[3].toString());
+
+        // for (String s : sets) {
+        //     System.out.println(s);
+        // }
+        // String sarr[]=new String[sets.size()];
+        // sets.toArray(sarr);
+
+        // for (int i = 0; i < sarr.length; i++) {
+        //     System.out.println(sarr[i]);
+        // }
+
+        // Set<String> sets=new TreeSet<>();
+
+        // sets.add("Aman");
+        // sets.add("Dolly");
+        // sets.add("Lalit");
+        // sets.add("prince");
+        // sets.add("Aman 2");
+        // sets.add("Aman");
+        // sets.add("Aman");
+        // sets.add("piyush");
+
+        // System.out.println(sets);
+
+        LinkedHashSet<String> sets=new LinkedHashSet<>();
+        sets.add("Aman");
+        sets.add("Dolly");
+        sets.add("Lalit");
+        sets.add("prince");
+        sets.add("Aman 2");
+        sets.add("Aman");
+        sets.add("Aman");
+        sets.add("piyush");
+        
+        System.out.println(sets);
+
+    }
+}
